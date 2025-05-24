8a9dec8b9534280ecd854e595887cc13b2f781f2
muzudho <muzudho1@gmail.com>
2025-05-24 12:08:04 +0900

    ネームスペース

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,16 @@
 
 //use tauri_plugin_fs::FsExt;
 use sha2::{Digest, Sha256};
-use std::fs;
-use std::io;
+//use std::fs;
+//use std::fs::File;
+//use std::io;
+//use std::io::Read;
 
 //*
 #[tauri::command]
-fn scan_files(path: String) -> Vec<String> {
+fn scan_files_1(path: String) -> Vec<String> {
     let mut files = Vec::new();
-    if let Ok(entries) = fs::read_dir(path) {
+    if let Ok(entries) = std::fs::read_dir(path) {
         for entry in entries {
             if let Ok(file) = entry {
                 let path = file.path();
@@ -24,27 +26,55 @@ fn scan_files(path: String) -> Vec<String> {
 }
 // */
 
+/*
+#[tauri::command]
+fn scan_files_2(app: tauri::AppHandle, path: String) -> Result<Vec<String>, String> {
+    let paths = app
+        .fs()
+        .read_dir(&path, None)
+        .map_err(|e| e.to_string())?
+        .into_iter()
+        .filter(|e| e.path().is_file())
+        .map(|e| e.path().to_string_lossy().to_string())
+        .collect();
+    Ok(paths)
+}
+// */
+
 //*
 #[tauri::command]
-fn get_file_hash(path: String) -> String {
-    let mut file = fs::File::open(&path).unwrap();
+fn get_file_hash_1(path: String) -> String {
+    let mut file = std::fs::File::open(&path).unwrap();
     let mut hasher = Sha256::new();
-    io::copy(&mut file, &mut hasher).unwrap();
+    std::io::copy(&mut file, &mut hasher).unwrap();
     format!("{:x}", hasher.finalize())
+}
+// */
+
+/*
+#[tauri::command]
+fn get_file_hash_2(app: tauri::AppHandle, path: String) -> Result<String, String> {
+    let mut file = File::open(&path).map_err(|e| e.to_string())?;
+    let mut hasher = Sha256::new();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).map_err(|e| e.to_string())?;
+    hasher.update(&buffer);
+    let result = hasher.finalize();
+    Ok(format!("{:x}", result))
 }
 // */
 
 //*
 #[tauri::command]
 fn delete_file(path: String) -> bool{
-    fs::remove_file(path).is_ok()
+    std::fs::remove_file(path).is_ok()
 }
 // */
 
 //*
 #[tauri::command]
 fn move_file(src: String, dest: String) -> bool{
-    fs::rename(src, dest).is_ok()
+    std::fs::rename(src, dest).is_ok()
 }
 // */
 
@@ -60,8 +90,8 @@ fn main() {
     //*
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            scan_files,
-            get_file_hash,
+            scan_files_1,
+            get_file_hash_1,
             delete_file,
             move_file
         ])

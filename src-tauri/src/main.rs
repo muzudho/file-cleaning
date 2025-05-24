c87850a9fc3b2a9235ef611a066aa2a74d064a0a
muzudho <muzudho1@gmail.com>
2025-05-24 11:42:47 +0900

    イベントハンドラ

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-/*
+//use tauri_plugin_fs::FsExt;
 use sha2::{Digest, Sha256};
 use std::fs;
 use std::io;
-*/
 
-/*
+//*
 #[tauri::command]
 fn scan_files(path: String) -> Vec<String> {
     let mut files = Vec::new();
@@ -23,23 +22,34 @@ fn scan_files(path: String) -> Vec<String> {
     }
     files
 }
+// */
 
+//*
 #[tauri::command]
 fn get_file_hash(path: String) -> String {
     let mut file = fs::File::open(&path).unwrap();
     let mut hasher = Sha256::new();
     io::copy(&mut file, &mut hasher).unwrap();
-    format!("{:x", hasher.finalize())
+    format!("{:x}", hasher.finalize())
 }
+// */
 
+//*
 #[tauri::command]
-fn delete_file(src: String, dest: String) -> bool{
+fn delete_file(path: String) -> bool{
+    fs::remove_file(path).is_ok()
+}
+// */
+
+//*
+#[tauri::command]
+fn move_file(src: String, dest: String) -> bool{
     fs::rename(src, dest).is_ok()
 }
-*/
+// */
 
 fn main() {
-    //*
+    /*
     file_cleaning_lib::run()
     // */
     /*
@@ -47,7 +57,7 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
     // */
-    /*
+    //*
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             scan_files,
@@ -56,6 +66,6 @@ fn main() {
             move_file
         ])
         .run(tauri::generate_context!())
-        .expect("Error running Tauri app");
+        .expect("error while running tauri application");
     // */
 }

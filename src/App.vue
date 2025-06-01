f99c28017c3a555c04c84e5eb64619834a51c1f4
muzudho <muzudho1@gmail.com>
2025-06-01 12:55:50 +0900

    スタイル設定

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,30 @@
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
 
+// ビューモデル。
 const greetMsg = ref("");
-const name = ref("");
+const openFolderPathVM = ref("");
 
-async function greet() {
+// ［フォルダーを開く］ボタンクリック時。
+async function on_openFolderButton_clicked() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
+  greetMsg.value = await invoke("greet", { name: openFolderPathVM.value });
 }
 </script>
 
 <template>
   <main class="container">
 
-    <div class="flex items-center justify-center min-h-screen bg-gray-900 text-while">
-      <h1 class="text-4xl font-bold text-fluent-blue">Hello, world!!</h1>
-    </div>
+    <h1>ファイル・クリーニング</h1>
 
-    <h1>Welcome to Tauri + Vue</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
+    <h2>手順１：</h2>
+    <!-- class="row" -->
+    <form @submit.prevent="on_openFolderButton_clicked">
+      <p class="p2">フォルダーを選択してください。</p>
+      <p class="p2">
+        <input id="open-folder-path-input" v-model="openFolderPathVM" placeholder="Enter a folder path..." value="Z:\\Muzudho Backup" />
+        <button type="submit">フォルダーを開く</button>
+      </p>
     </form>
     <p>{{ greetMsg }}</p>
   </main>
@@ -75,7 +65,7 @@ async function greet() {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  text-align: center;
+  text-align: left;
 }
 
 .logo {
@@ -105,7 +95,24 @@ a:hover {
 }
 
 h1 {
-  text-align: center;
+  background-color: #111111;
+  color: #EEEEEE;
+  padding-top: 8px;
+  padding-bottom: 4px;
+}
+
+h2 {
+  background-color: #111111;
+  color: #EEEEEE;
+  margin-left: 60px;
+  padding-left: 4px;
+  padding-top: 6px;
+  padding-bottom: 2px;
+}
+
+p.p2 {
+  color: #111111;
+  margin-left: 90px;
 }
 
 input,
